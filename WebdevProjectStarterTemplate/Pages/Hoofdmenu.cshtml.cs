e202b01662ba83c02a64007e414a2e29bfe1b955
JoostBakker28 <joost.bakker@student.nhlstenden.com>
2023-03-06 15:36:56 +0100

    commit1

---
+++ WebdevProjectStarterTemplate/Pages/Hoofdmenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebdevProjectStarterTemplate.Pages
+{
+    public class HoofdmenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
