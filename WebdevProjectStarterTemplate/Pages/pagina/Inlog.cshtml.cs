a01e2caa2b8e7b31b41ba200f75060abf7419a26
JoostBakker28 <113503680+JoostBakker28@users.noreply.github.com>
2023-03-20 14:04:27 +0100

    Routing toegevoegd

--- WebdevProjectStarterTemplate/Pages/pagina/Inlog.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/pagina/Inlog.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 using System.Diagnostics.Eventing.Reader;
 
 namespace WebdevProjectStarterTemplate.Pages.pagina
 {
     public class InlogModel : PageModel
     {
+        [BindProperty]
+        public string Email { get; set; }
+        [BindProperty]
+        public string Wachtwoord { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if(Email == "Joost@test.nl" && Wachtwoord == "1234")
+            {
+				return new RedirectToPageResult("/Pagina/OberPagina");
+			}
+            else
+            {
+                return Page();
+            }
+        }
         public void OnGet()
         {
 
---
+++ WebdevProjectStarterTemplate/Pages/pagina/OberPagina.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebdevProjectStarterTemplate.Pages.pagina
+{
+    public class OberPaginaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
