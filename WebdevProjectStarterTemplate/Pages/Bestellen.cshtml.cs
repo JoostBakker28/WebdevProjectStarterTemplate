a7ee76fc9a1f642bd79c59eed2b7a4ceef9e76f8
JoostBakker28 <joost.bakker@student.nhlstenden.com>
2023-10-27 14:48:48 +0200

    wip

--- WebdevProjectStarterTemplate/Pages/Bestellen.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/Bestellen.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Diagnostics;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySqlX.XDevAPI.Relational;
 using System.Text.RegularExpressions;
 using WebdevProjectStarterTemplate.Models;
 using WebdevProjectStarterTemplate.Repositories;
@@ -10,44 +11,34 @@ namespace WebdevProjectStarterTemplate.Pages
     public class BestellenModel : PageModel
     {
 
-        public IEnumerable<Table> TableList { get { return new TableRepository().Get(); } }//Haal alle tafels op en zet ze in een lijst
+        public IEnumerable<Models.Table> TableList { get { return new TableRepository().Get(); } }//Haal alle tafels op en zet ze in een lijst
         public IEnumerable<Category> Categories { get { return new CategoryRepository().GetCategoriesWithProducts(); } }
         public IEnumerable<Order> Orders { get
             {
                 string result;
-                result = Regex.Match(Convert.ToString(SelectedTableID), @"/d+").Value;
+                result = Regex.Match(SelectedTableID, @"\d+").Value;
                 return new OrderRepository().Get(Int32.Parse(result));
             } }
 
         [BindProperty]
-        public string SelectedTableID { get; set; }
+        public string? SelectedTableID { get; set; }
         [BindProperty(SupportsGet = true)]
         public string Category { get; set; } = null!;
 
-        public IActionResult OnPost()
-        {
-            if (SelectedTableID != null)
-            {
-                HttpContext.Session.SetString("TableID", SelectedTableID);
-                Response.Cookies.Append("SelectedTable", SelectedTableID);
-            }
-            return Page(); 
-        }
 
-        public IActionResult OnGet() 
+        public IActionResult OnGet(string table) 
         {
-            if(this.SelectedTableID != null)
+            if (this.SelectedTableID != null) 
             {
-                Response.Cookies.Append("SelectedTable", SelectedTableID);
+                Response.Cookies.Append("MyTable", table); 
                 return Page();
-
             }
-            else if(this.SelectedTableID == null && Request.Cookies["SelectedTable"] != null)
+            else if (this.SelectedTableID == null && Request.Cookies["MyTable"] != null) 
             {
-                this.SelectedTableID = Request.Cookies["SelectedTable"];
-                return RedirectToPage("/Bestellen", new {SelectedTableID });
+                this.SelectedTableID = Request.Cookies["MyTable"]; 
+                return RedirectToPage("/Bestellen", new { table = this.SelectedTableID }); 
             }
-            else
+            else 
             {
                 return Page();
             }
